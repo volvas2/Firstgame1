13542b0349ee45023f64069127e985651e823cbc
Artur <AshesWAr@mail.ru>
2024-01-30 16:20:53 +0300

    Rain 3D

---
+++ Assets/Lessons/Rain 3D Objects/Scripts/DestroyObjectController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Lessons.Rain_3D_Objects
+{
+    public class DestroyObjectController
+    {
+        private readonly Transform _transform;
+
+        public DestroyObjectController(Transform transform)
+        {
+            _transform = transform;
+        }
+
+        public void Check()
+        {
+            if (_transform.position.y < -5)
+            {
+                Object.Destroy(_transform.gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/Lessons/Rain 3D Objects/Scripts/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Lessons.Rain_3D_Objects
+{
+    public class Shoot : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _mask;
+        
+        private Camera _camera;
+        
+        private const int RADIUS_AREA_PHYSICS = 10;
+        private const int MAX_DISTANCE_RAY = 100;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                if(Physics.Raycast(ray, out RaycastHit hit, MAX_DISTANCE_RAY, _mask))
+                {
+                    Collider[] colliders = Physics.OverlapSphere(hit.point, RADIUS_AREA_PHYSICS);
+                    foreach (var collider in colliders)
+                    {
+                        collider.TryGetComponent(out ObjectView view);
+                        view?.AddImpulse();
+                    }
+                }
+            }
+        }
+    }
+}
